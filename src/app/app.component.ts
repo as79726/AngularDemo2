df9ab13212e253860502b3946f24c86fb7e8d967
andy.chen <as79726@gmail.com>
2017-11-25 16:05:35 +0800

    任務 14：實作全選功能

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,38 @@
-import { Component } from '@angular/core';
-import { FilterPipe } from './filter.pipe';
+import { Component } from "@angular/core";
+import { FilterPipe } from "./filter.pipe";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  inputHint: string ="What needs to be done?";
+  inputHint: string = "What needs to be done?";
   todos = new Array();
   todo: string = "";
   filterType = "All";
+  isToggleAll = false;
 
-  pushInputValue(value){
+  pushInputValue(value) {
     let newTodo = {
-      text:value,
-      done:false
+      text: value,
+      done: false
     };
     this.todos = this.todos.concat(newTodo);
     this.todo = "";
   }
 
-  clearCompleted($event){
+  clearCompleted($event) {
     this.todos = this.todos.filter(item => item.done === false);
   }
 
-  filterTypeChange($event){
+  filterTypeChange($event) {
     this.filterType = $event;
+  }
+
+  toggleAllChange() {
+    this.todos.forEach(item => {
+      item.done = this.isToggleAll;
+    });
   }
 }
