aa7bc0d939276f104624a907a323d2a1efe92345
andy.chen <as79726@gmail.com>
2017-11-25 10:10:37 +0800

    任務 03：切割網頁為多個子元件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TitleComponentComponent } from './title-component/title-component.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleComponentComponent
   ],
   imports: [
     BrowserModule
